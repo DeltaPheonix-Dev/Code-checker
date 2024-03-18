597830213dfffb2233d34a6d7f3949141eec416d
Craig Underdown <81975181+DeltaPheonix-Dev@users.noreply.github.com>
2024-03-18 20:27:21 +0000

    Updated Server.js

--- Server.js
+++ Server.js
@@ -5,6 +5,8 @@ const { body, validationResult } = require('express-validator');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname));
+
 app.use(express.json());
 
 /*
